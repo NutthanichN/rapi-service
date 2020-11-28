f8bcbe8739fbdbc459e743d04f291ef3131e8c4e
Nutthanich Narphromar <monaprom134@gmail.com>
2020-11-28 09:08:17 +0700

    Complete db models

--- rapi_site/models.py
+++ rapi_site/models.py
@@ -1,30 +1,50 @@
-from sqlalchemy import Table, Column, Integer, String
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
 from rapi_site.database import Base
 
 
 class Restaurant(Base):
-    # TODO: complete all columns
     __tablename__ = 'restaurant'
     id = Column(Integer, primary_key=True)
-    name = Column(String(50), unique=True)
+    name = Column(String(255))
+    latitude = Column(String(10), nullable=False)
+    longitude = Column(String(10), nullable=False)
+    opening_hour = Column(String(15))
+    wongnai_rating = Column(Float)
+    tripadvisor_rating = Column(Float)
+    address = Column(String(255))
+    cuisine_id = Column(Integer, ForeignKey('cuisine.id', ondelete='CASCADE'))
+    district_id = Column(Integer, ForeignKey('district.id', ondelete='CASCADE'))
 
-    def __init__(self, name=None):
-        self.name = name
+    def __repr__(self):
+        return f"<Restaurant(name='{self.name}', lat='{self.latitude}', long='{self.longitude}', opening_hour='{self.opening_hour}', wongnai_rating='{self.wongnai_rating}', tripadvisor_rating='{self.tripadvisor_rating}, address='{self.address}, cuisine_id='{self.cuisine_id}')>"
 
 
 class District(Base):
     __tablename__ = 'district'
     id = Column(Integer, primary_key=True)
-    name = Column(String(50), unique=True)
+    name = Column(String(255))
 
-    def __init__(self, name=None):
-        self.name = name
+    restaurant = relationship(
+        'Restaurant',
+        cascade='all, delete',
+        passive_deletes=True
+    )
+
+    def __repr__(self):
+        return f"<District(name='{self.name}')>"
 
 
 class Cuisine(Base):
     __tablename__ = 'cuisine'
     id = Column(Integer, primary_key=True)
-    name = Column(String(50), unique=True)
+    name = Column(String(255))
 
-    def __init__(self, name=None):
-        self.name = name
+    restaurant = relationship(
+        'Restaurant',
+        cascade='all, delete',
+        passive_deletes=True
+    )
+
+    def __repr__(self):
+        return f"<Cuisine(name='{self.name}')>"
