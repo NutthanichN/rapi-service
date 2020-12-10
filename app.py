ab7a87cc8142fd87d9602fab3a4651dfd463483a
Nutthanich Narphromar <monaprom134@gmail.com>
2020-12-11 03:55:01 +0700

    Combine two flask apps to one wsgi app

--- app.py
+++ app.py
@@ -2,6 +2,25 @@
 Run this file to start rapi_site and api server
 """
 from rapi_site import create_app
+from rapi import create_app as create_api_app
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from werkzeug.serving import run_simple
 
+rapi_service = create_api_app()
 rapi_site = create_app()
-rapi_site.run(debug=True)
+
+application = DispatcherMiddleware(
+    rapi_service, {
+        '/rapi-site': rapi_site
+    }
+)
+
+if __name__ == '__main__':
+    run_simple(
+        hostname='localhost',
+        port=5000,
+        application=application,
+        use_reloader=True,
+        use_debugger=True,
+        use_evalex=True
+    )
