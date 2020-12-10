5d47ca4471af7c77bd998d0ddd0dd76723cc7376
Nutthanich Narphromar <monaprom134@gmail.com>
2020-12-11 04:07:46 +0700

    Add controllers for each endpoint

--- rapi/controller.py
+++ rapi/controller.py
@@ -1,10 +1,12 @@
+from sqlalchemy.orm.exc import NoResultFound
+
 from rapi.autogen.openapi_server import models
 from rapi_site.database import db_session
 from flask import abort
 from rapi_site.models import Restaurant, Cuisine, District
 
 
-def get_restaurant():
+def get_restaurants():
     restaurants = db_session.query(Restaurant, Cuisine).join(Cuisine).filter(Restaurant.cuisine_id == Cuisine.id).all()
 
     results = [
@@ -13,131 +15,140 @@ def get_restaurant():
             cuisine_name=r[1].name, opening_hour=f"{r[0].open_time}-{r[0].close_time}",
             google_rating=r[0].google_rating,
             tripadvisor_rating=r[0].tripadvisor_rating,
-            address=r[0].address
+            address=r[0].address, michelin_star=r[0].michelin_star
         ) for r in restaurants
     ]
     return results
 
 
 def get_restaurant_details(restaurantId):
-    restaurant = db_session.query(Restaurant).filter(Restaurant.id == restaurantId).one()
-    cuisine = db_session.query(Cuisine).filter(Cuisine.id == restaurant.cuisine_id).one()
+    try:
+        restaurant = db_session.query(Restaurant).filter(Restaurant.id == restaurantId).one()
+        cuisine = db_session.query(Cuisine).filter(Cuisine.id == restaurant.cuisine_id).one()
 
-    result = models.Restaurant(
-        id=restaurant.id, name=restaurant.name, location=[restaurant.latitude, restaurant.longitude],
-        cuisine_name=cuisine.name, opening_hour=f"{restaurant.open_time}-{restaurant.close_time}",
-        google_rating=restaurant.google_rating,
-        tripadvisor_rating=restaurant.tripadvisor_rating,
-        address=restaurant.address
-    )
-    return result
+        result = models.Restaurant(
+            id=restaurant.id, name=restaurant.name, location=[restaurant.latitude, restaurant.longitude],
+            cuisine_name=cuisine.name, opening_hour=f"{restaurant.open_time}-{restaurant.close_time}",
+            google_rating=restaurant.google_rating,
+            tripadvisor_rating=restaurant.tripadvisor_rating,
+            address=restaurant.address, michelin_star=restaurant.michelin_star
+        )
+        return result
+    except NoResultFound:
+        abort(404)
 
 
-# def get_michelin_restaurant():
-#     restaurants = db_session.query(Restaurant, Cuisine).join(Cuisine).filter(Restaurant.cuisine_id == Cuisine.id).all()
-#
-#     print(restaurants)
-#
-#     results = [
-#         models.Restaurant(
-#             id=r[0].id, name=r[0].name, location=[r[0].latitude, r[0].longitude],
-#             cuisine_name=r[1].name, opening_hour=f"{r[0].open_time}-{r[0].close_time}",
-#             google_rating=r[0].google_rating,
-#             tripadvisor_rating=r[0].tripadvisor_rating,
-#             address=r[0].address
-#         ) for r in restaurants
-#     ]
-#     return results
-#
-#
-# print(get_michelin_restaurant())
+def get_michelin_restaurants():
+    restaurants = db_session.query(Restaurant, Cuisine).join(Cuisine)\
+        .filter(Restaurant.cuisine_id == Cuisine.id)\
+        .filter(Restaurant.michelin_star > 0).all()
+
+    # restaurants_distinct = []
+    # restaurant_names = []
+    # for r in restaurants:
+    #     if r.name not in restaurant_names:
+    #         restaurants_distinct.append(r)
+    #         restaurant_names.append(r.name)
+
+    results = [
+        models.Restaurant(
+            id=r[0].id, name=r[0].name, location=[r[0].latitude, r[0].longitude],
+            cuisine_name=r[1].name, opening_hour=f"{r[0].open_time}-{r[0].close_time}",
+            google_rating=r[0].google_rating,
+            tripadvisor_rating=r[0].tripadvisor_rating,
+            address=r[0].address, michelin_star=r[0].michelin_star
+        ) for r in restaurants
+    ]
+
+    return results
 
 
 def get_restaurant_google_rating(restaurantId):
-    restaurant = db_session.query(Restaurant).filter(Restaurant.id == restaurantId).one()
-    cuisine = db_session.query(Cuisine).filter(Cuisine.id == restaurant.cuisine_id).one()
-
-    result = models.Restaurant(
-        id=restaurant.id, name=restaurant.name, location=[restaurant.latitude, restaurant.longitude],
-        cuisine_name=cuisine.name, opening_hour=f"{restaurant.open_time}-{restaurant.close_time}",
-        google_rating=restaurant.google_rating,
-        tripadvisor_rating=restaurant.tripadvisor_rating,
-        address=restaurant.address
-    )
-    return result.google_rating
+    try:
+        restaurant = db_session.query(Restaurant).filter(Restaurant.id == restaurantId).one()
+        return restaurant.google_rating
+    except NoResultFound:
+        abort(404)
 
 
 def get_restaurant_tripadvisor_rating(restaurantId):
-    restaurant = db_session.query(Restaurant).filter(Restaurant.id == restaurantId).one()
-    cuisine = db_session.query(Cuisine).filter(Cuisine.id == restaurant.cuisine_id).one()
-
-    result = models.Restaurant(
-        id=restaurant.id, name=restaurant.name, location=[restaurant.latitude, restaurant.longitude],
-        cuisine_name=cuisine.name, opening_hour=f"{restaurant.open_time}-{restaurant.close_time}",
-        google_rating=restaurant.google_rating,
-        tripadvisor_rating=restaurant.tripadvisor_rating,
-        address=restaurant.address
-    )
-    return result.tripadvisor_rating
+    try:
+        restaurant = db_session.query(Restaurant).filter(Restaurant.id == restaurantId).one()
+        return restaurant.tripadvisor_rating
+    except NoResultFound:
+        abort(404)
 
 
-# def get_district():
-#     districts = db_session.query(District).all()
-#
-#     print(districts)
-#
-#     results = [
-#         models.District(
-#             id=d[1].id, name=d[1].name
-#         ) for d in districts
-#     ]
-#     return results
-#
-# print(get_district())
+def get_districts():
+    districts = db_session.query(District).all()
+
+    results = [
+        models.District(id=d.id, name=d.name) for d in districts
+    ]
+    return results
 
 
-# def get_restaurant_by_district(districtId):
-#     district = db_session.query(District, Restaurant).join(Restaurant).filter(Restaurant.district_id == District.Id).filter(District.id == districtId).one()
-#
-#     print(district)
-#
-#     # TODO: update .yaml (wongnai_rating -> google_rating)
-#     result = models.District(
-#         id=district.id, name=district.name,
-#     )
-#     return result
-#
-#
-# print(get_restaurant_by_district(51))
+def get_district_by_id(districtId):
+    try:
+        district = db_session.query(District).filter(District.id == districtId).one()
+        return models.District(id=district.id, name=district.name)
+    except NoResultFound:
+        abort(404)
 
 
-# def get_cuisine():
-#     cuisines = db_session.query(Cuisine).all()
-#
-#     # TODO: update .yaml (wongnai_rating -> google_rating)
-#     results = [
-#         models.Cuisine(
-#             id=c[0].id, name=c[0].name
-#         ) for c in cuisines
-#     ]
-#     return results
+def get_restaurants_by_district(districtId):
+    restaurants = db_session.query(Restaurant, Cuisine).join(Cuisine)\
+        .filter(Restaurant.cuisine_id == Cuisine.id)\
+        .filter(Restaurant.district_id == districtId).all()
+
+    # restaurants_distinct = []
+    # restaurant_names = []
+    # for r in restaurants:
+    #     if r[0].name not in restaurant_names:
+    #         restaurants_distinct.append(r)
+    #         restaurant_names.append(r[0].name)
+
+    results = [
+        models.Restaurant(
+            id=r[0].id, name=r[0].name, location=[r[0].latitude, r[0].longitude],
+            cuisine_name=r[1].name, opening_hour=f"{r[0].open_time}-{r[0].close_time}",
+            google_rating=r[0].google_rating,
+            tripadvisor_rating=r[0].tripadvisor_rating,
+            address=r[0].address, michelin_star=r[0].michelin_star
+        ) for r in restaurants
+    ]
+    return results
 
 
-def get_specified_cuisine(cuisineId):
-    cuisine = db_session.query(Cuisine).filter(Cuisine.id == cuisineId).one()
+def get_cuisines():
+    cuisines = db_session.query(Cuisine).all()
 
-    # TODO: update .yaml (wongnai_rating -> google_rating)
-    result = models.Cuisine(
-        id=cuisine.id, name=cuisine.name,
-    )
-    return result
+    results = [
+        models.Cuisine(id=c.id, name=c.name) for c in cuisines
+    ]
+    return results
 
 
-# def get_specified_cuisine_restaurant(cuisineId):
-#     cuisine = db_session.query(Cuisine, Restaurant).join(Restaurant).filter(Restaurant.cuisine_id == Cuisine.id).filter(Cuisine.id == cuisineId).one()
-#
-#     # TODO: update .yaml (wongnai_rating -> google_rating)
-#     result = models.District(
-#         id=cuisine.id, name=cuisine.name,
-#     )
-#     return result
+def get_cuisine_by_id(cuisineId):
+    try:
+        cuisine = db_session.query(Cuisine).filter(Cuisine.id == cuisineId).one()
+        return models.Cuisine(id=cuisine.id, name=cuisine.name)
+    except NoResultFound:
+        abort(404)
+
+
+def get_restaurants_by_cuisine(cuisineId):
+    restaurants = db_session.query(Restaurant, Cuisine).join(Cuisine)\
+        .filter(Restaurant.cuisine_id == Cuisine.id)\
+        .filter(Cuisine.id == cuisineId).all()
+
+    results = [
+        models.Restaurant(
+            id=r[0].id, name=r[0].name, location=[r[0].latitude, r[0].longitude],
+            cuisine_name=r[1].name, opening_hour=f"{r[0].open_time}-{r[0].close_time}",
+            google_rating=r[0].google_rating,
+            tripadvisor_rating=r[0].tripadvisor_rating,
+            address=r[0].address, michelin_star=r[0].michelin_star
+        ) for r in restaurants
+    ]
+    return results
