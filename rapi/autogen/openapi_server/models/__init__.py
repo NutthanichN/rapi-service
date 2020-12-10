0aec7df138d041cfd6fda75b4d1bd954ac637121
Nutthanich Narphromar <monaprom134@gmail.com>
2020-12-11 01:59:10 +0700

    Fix import error in __init__.py of openapi_server models

--- rapi/autogen/openapi_server/models/__init__.py
+++ rapi/autogen/openapi_server/models/__init__.py
@@ -3,6 +3,6 @@
 # flake8: noqa
 from __future__ import absolute_import
 # import models into model package
-from openapi_server.models.cuisine import Cuisine
-from openapi_server.models.district import District
-from openapi_server.models.restaurant import Restaurant
+from rapi.autogen.openapi_server.models.cuisine import Cuisine
+from rapi.autogen.openapi_server.models.district import District
+from rapi.autogen.openapi_server.models.restaurant import Restaurant
